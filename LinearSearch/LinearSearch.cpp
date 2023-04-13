005c05a8d1c21792f2a96559bd4ce84963daf17c
iqwa1107 <ramahs49@gmail.com>
2023-04-13 14:43:55 +0700

    Accept array element

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -18,4 +18,13 @@ void input()
 			cout << "\nArray should have minimum 1 and maximum 20 elemnts. \n\n";
 		
 	}
+	// Accept array element 
+	cout << "\n------------------\n";
+	cout << " Enter aray Elmenet \n";
+	cout << "---------------------\n";
+	for ( i = 0; i < n; i++)
+	{
+		cout << "<" << (i + i) << "> ";
+		cin >> arr[i];
+	}
 }
